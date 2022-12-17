51e294025d77e8b06f0df3410245221ff5997bb2
shyusu4 <shyusupova4@gmail.com>
2022-12-17 19:12:37 +0500

    Toggle badges

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const notifsCounter = document.getElementById('notifs-counter');
 const markAll = document.getElementById('mark-all');
+
+function toggleBadges() {
+  const dots = document.getElementsByClassName('dot');
+  for (const dot of dots) {
+    if (dot.style.display === 'none') {
+      dot.style.display = 'inline-block';
+    } else {
+      dot.style.display = 'none';
+    }
+  }
+}
